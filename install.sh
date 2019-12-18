0525315825d494044e00e1fa8e9c152944fa5da4
convict-git <official.mr.convict@gmail.com>
2019-12-18 17:23:08 +0530

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo cp -v bin/compile_cp_parser_cli-linux /usr/bin/cp-compile
+sudo cp -v bin/cp_parser_cli-linux /usr/bin/cp-parse
+sudo cp -v bin/tester_cp_parser_cli-linux /usr/bin/cp-tester
