5e279121e994f742f2be6816262430227cee0030
convict-git <official.mr.convict@gmail.com>
2020-02-28 07:11:22 +0530

    Added change mode to executable commands in install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@
 sudo cp -v bin/compile_cp_parser_cli-linux /usr/bin/cp-compile
 sudo cp -v bin/cp_parser_cli-linux /usr/bin/cp-parse
 sudo cp -v bin/tester_cp_parser_cli-linux /usr/bin/cp-tester
+sudo chmod +x /usr/bin/cp-compile
+sudo chmod +x /usr/bin/cp-parse
+sudo chmod +x /usr/bin/cp-tester
+
