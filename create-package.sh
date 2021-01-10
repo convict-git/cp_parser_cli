065b10b53ca1091fda8beaeb60d2fbc1a3a357a0
convict-git <official.mr.convict@gmail.com>
2021-01-11 03:43:32 +0530

    Added script to create package.zip automatically as release

---
+++ create-package.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/bash
+
+if
+   mkdir -p package &&
+   cp -v ./install.sh ./package &&
+   cp -v ./bin/cp-parse ./package/bin &&
+   cp -rv ./scripts ./package
+then
+   echo "created a temp directory for package..."
+else
+   rm -rfv ./package
+   echo -e "\e[31mPackage creation failed!\e[0m"
+   exit
+fi
+
+if zip -rv package.zip ./package;
+then
+   echo -e "\e[32mPackage created as package.zip!\e[0m"
+else
+   echo -e "\e[31mPackage creation failed!\e[0m"
+fi
+rm -rfv ./package
