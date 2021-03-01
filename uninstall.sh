94c49f3a21afd0beace5bc25e38a18b6c828c5b2
convict-git <official.mr.convict@gmail.com>
2021-03-01 16:19:31 +0530

    Added about hcscript in uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -5,6 +5,7 @@ if sudo rm -rfv /usr/bin/cp-parse &&
    sudo rm -rfv /usr/bin/cp-compile &&
    sudo rm -rfv /usr/bin/cp-random &&
    sudo rm -rfv /usr/bin/cp-tester &&
+   sudo rm -rfv /usr/bin/cp-hcscript &&
    sudo rm -rfv ~/.config/cpparsercli/config.json
 then
    echo -e "\e[1m\e[32mUninstalled successfully. I hope you enjoyed!\e[0m"
